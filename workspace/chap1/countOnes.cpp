791aa43940215dab4341c3228b17dd7f1aa3bd65
Jiang Haiyun <jiang.haiyun@gmail.com>
2018-03-03 18:03:41 +0800

    countOnes new version

--- workspace/chap1/countOnes.cpp
+++ workspace/chap1/countOnes.cpp
@@ -1,5 +1,5 @@
-#include <stdbool.h>
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 
 int countOnes(unsigned int n) { // 统计整数 n 的二进制展开中数位 1 的总数： O(logn)
@@ -11,7 +11,26 @@ int countOnes(unsigned int n) { // 统计整数 n 的二进制展开中数位 1 
     return ones;
 } // 等效于 glibc 的内置函数 int __builtin_popcount(unsigned int n)
 
+
+/* 对于任意整数 n，不妨设其最低（右）为数位 1 对应 2^k，则最低的 k+1 位必然是 "100...0"，即
+ * 数位 1 后是 k 个 0。
+ * 而 n-1 的二进制展开后，其第 k+1 位是 0, 而低的 k 位都是 1，从而 n & (n-1) 后低 k+1 位都为 0.
+ * 改进版本：操作次数线性正比于 n 的二进制展开中数位 1 的实际数目。
+ */
+
+int countOnes1(unsigned int n) { // O(ones) 正比于数位 1 的总数
+    int ones = 0;
+    while (n>0) {
+        ones ++; //计数，至少有一位为 1
+        n &= n-1;
+    }
+    return ones;
+}
+
 int main() {
     unsigned int n = 441; // 110111001
-    printf("Ones of 441: %d\n", countOnes(n));
+    cout << "countOnes(441): " << countOnes(114) << endl;
+    cout << "countOnes1(441): " << countOnes1(114) << endl;
+
+    return 0;
 }
